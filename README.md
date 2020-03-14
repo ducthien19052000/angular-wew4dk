c5ddf08b0ef718641c2c6a7f007bebe72834052c
ducthien19052000 <ducthien19052000@gmail.com>
2020-03-14 18:05:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wew4dk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wew4dk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SlideComponent } from './slide/slide.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, ProductComponent, ProductListComponent, SlideComponent, CategoryComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
